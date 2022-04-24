691f2982f3a82b9070e4e385b0d9e611f56f0cf9
Iryna Karavaichyk <iryna.karavaichyk@iterxgroup.com>
2022-04-25 02:12:18 +0400

    tests: add tests

---
+++ src/tests/mocks/key-value/storage.mock.js
@@ -0,0 +1,9 @@
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+import sinon from 'sinon';
+
+export default sinon.stub({
+  addValue: () => {},
+  getValue: () => {},
+  removeValue: () => {},
+  clear: () => {},
+});
---
+++ src/tests/mocks/stack/storage.mock.js
@@ -0,0 +1,8 @@
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+import sinon from 'sinon';
+
+export default sinon.stub({
+  addItem: () => {},
+  getItem: () => {},
+  clear: () => {},
+});
